e256121e0764562425846955dd3121eae9c81f5b
saranya-shanmugam <saranya.pshanmugam@gmail.com>
2017-06-04 13:33:36 -0500

    Initialized StringBuilder up-front to avoid doubling capacity

--- src/com/ttu/algorithms/StringCompression.java
+++ src/com/ttu/algorithms/StringCompression.java
@@ -6,46 +6,52 @@ import java.util.LinkedHashMap;
 public class StringCompression {
 	public static void main(String[] args) {
 		StringCompression stringCompression = new StringCompression();
-		stringCompression.getCompressedString("aabcccccaaa"); //output: a2b1c5a3
-		stringCompression.getCompressedString("abcd"); //output: abcd
+		stringCompression.getCompressedString("aabcccccaaa"); // output:
+																// a2b1c5a3
+		stringCompression.getCompressedString("abcd"); // output: abcd
 	}
-	
+
 	private void getCompressedString(String str1) {
-		Character ch,  prevCh = str1.charAt(0);
+		Character ch, prevCh = str1.charAt(0);
 		Integer count;
 		boolean isProcessString = isProcessString(str1);
-		if(!isProcessString) {
+		if (!isProcessString) {
 			System.out.println(str1);
 			return;
 		}
-		StringBuilder compressedString = new StringBuilder();
+		// We can initialize StringBuilder to its necessary capacity up-front.
+		// Without this, StringBuilder will (behind the scenes) need to double
+		// its capacity every time it hits capacity. The capacity could be
+		// double what we ultimately need.
+		StringBuilder compressedString = new StringBuilder(str1.length());
 		LinkedHashMap<Character, Integer> charCount = new LinkedHashMap<>();
 		for (int i = 0; i < str1.length(); i++) {
 			ch = str1.toLowerCase().charAt(i);
 			count = charCount.get(ch);
 			if (count != null) {
-				charCount.put(ch, count+1);
+				charCount.put(ch, count + 1);
 			} else {
 				charCount.put(ch, 1);
 			}
 
-			if(prevCh != ch || i == str1.length()-1) {
+			if (prevCh != ch || i == str1.length() - 1) {
 				count = charCount.remove(prevCh);
 				compressedString.append(prevCh);
 				compressedString.append(count);
 			}
-			if(prevCh != ch && i == str1.length()-1) {
+			if (prevCh != ch && i == str1.length() - 1) {
 				count = charCount.remove(ch);
 				compressedString.append(ch);
 				compressedString.append(count);
 			}
-			prevCh = ch;	
+			prevCh = ch;
 		}
 		System.out.println(compressedString);
 	}
 
 	/**
-	 * Returns true if a character appears more than once in the given string so that the string will be compressed.
+	 * Returns true if a character appears more than once in the given string so
+	 * that the string will be compressed.
 	 * 
 	 * @param str1
 	 * @return
