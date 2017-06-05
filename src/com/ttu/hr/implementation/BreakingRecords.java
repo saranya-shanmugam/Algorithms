893cf483254ed84bfef5b9d0b683da71eef5d4b0
saranya-shanmugam <saranya.pshanmugam@gmail.com>
2017-06-04 22:57:55 -0500

    Checking best and worst score breaking counts

---
+++ src/com/ttu/hr/implementation/BreakingRecords.java
@@ -0,0 +1,38 @@
+package com.ttu.hr.implementation;
+
+import java.util.Scanner;
+
+public class BreakingRecords {
+
+	 public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt();
+	        int[] s = new int[n];
+	        for(int s_i=0; s_i < n; s_i++){
+	            s[s_i] = in.nextInt();
+	        }
+	        in.close();
+	        int[] result = getRecord(s);
+	        String separator = "", delimiter = " ";
+	        for (Integer val : result) {
+	            System.out.print(separator + val);
+	            separator = delimiter;
+	        }
+	        System.out.println("");
+	    }
+	 
+	 private static int[] getRecord(int[] s){
+		 int min = s[0], max = s[0];
+		 int minCount = 0, maxCount = 0;
+	        for(int i = 1; i < s.length; i++) {
+	        	if(s[i] > max) {
+	        		max = s[i];
+	        		maxCount ++;
+	        	} else if(s[i] < min) {
+	        		min = s[i];
+	        		minCount++;
+	        	}
+	        }
+	        return new int[]{maxCount, minCount};
+	    }
+}
